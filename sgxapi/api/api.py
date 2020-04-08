288723e54e5f46cb071b895ffcf7bf14dc26536a
Le Beier <moxlotus@live.com>
2020-04-08 19:03:11 +0800

    added first testcase

---
+++ sgxapi/api/api.py
@@ -0,0 +1,4 @@
+import requests
+
+def get_latest_price() -> None:
+    requests.get('https://www.sgx.com')
--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,2 +1,13 @@
-def test_sample():
-    assert 3 == 3
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+from sgxapi.api import get_latest_price
+
+class TestApi:
+
+    class TestGetLatestPrice:
+
+        @patch('requests.get')
+        def test_should_call_requests_get(self, mock_requests_get: MagicMock) -> None:
+            get_latest_price()
+            mock_requests_get.assert_called()
